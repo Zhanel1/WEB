3fe55a702862b8451c41fa0f21bbe53ec94776f6
Khalykova Zhanel Timurkyzy <z_khalykova@kbtu.kz>
2021-09-08 16:04:49 +0600

    Second commit

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -5,19 +5,51 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="circle"></div>
+          <div className="menu">
+            <a id="home" href="">Home</a>
+            <a id="blog" href="">Blog</a>
+            <a id="features" href="">Features</a>
+            <a id="pricing" href="">Pricing</a>
+            <a id="doc" href="">Documentation</a>
+          </div>
+          <div className="social media">
+
+          </div>
+          <div >
+            <div className="rectangle2"></div>
+            <button className="rectangle1">
+              Get started
+            </button>
+          </div>
       </header>
+      <main>
+        <hr/>
+          <div className="texts">        
+            <p className= "text1">PLAN YOUR LIFE</p>
+            <p className= "text2">Increase your <b>productivity</b></p>
+            <p className= "text3">Brute laoreet efficiendi id his, ea illum nonumes luptatum pro. 
+              Usu atqui laudem an, insolens gubergren similique est cu. 
+              Et vel modus congue vituperata.</p>
+          </div>  
+          <div className="clouds">
+            <div className="cloud1"></div>
+            <div className="cloud2"></div>
+            <div className="cloud3"></div>
+            <div className="cloud4"></div>
+            <div className="cloud5"></div>
+            <div className="cloud6"></div>
+            <div className="cloud7"></div>
+            <div className="cloud8"></div>
+            <div className="cloud9"></div>
+            <div className="cloud10"></div>
+            <div className="cloud11"></div>
+            <div className="cloud12"></div>
+          </div>
+            <div className="play"></div>
+            <div className="polygon"></div>
+        
+      </main>
     </div>
   );
 }
